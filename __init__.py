4b1d86891c6eb864c81f049bddcbf54257d7fdd7
Ghostkeeper <rubend@tutanota.com>
2017-09-07 18:49:01 +0200

    Update to Uranium's new plug-in API
    This moves most of the metadata out to a separate file plugin.json.

--- __init__.py
+++ __init__.py
@@ -1,21 +1,13 @@
-# Copyright (c) 2016 Ghostkeeper
-# Cura is released under the terms of the AGPLv3 or higher.
+# Copyright (c) 2017 Ghostkeeper
+# X3GWriter is released under the terms of the AGPLv3 or higher
 
 from . import X3GWriter
 
 from UM.i18n import i18nCatalog
-catalog = i18nCatalog("cura")
+catalog = i18nCatalog("x3gwriter")
 
 def getMetaData():
     return {
-        "plugin": {
-            "name": "X3G Writer",
-            "author": "Malyan",
-            "version": "1.0",
-            "description": catalog.i18nc("X3G Writer Plugin Description", "Writes X3G to a file"),
-            "api": 3
-        },
-
         "mesh_writer": {
             "output": [{
                 "extension": "x3g",
