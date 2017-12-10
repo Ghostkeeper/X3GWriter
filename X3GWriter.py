b1901d9310d980a9eaf54ccc5de9fde74070d309
Ruben D <rubend@tutanota.com>
2017-12-10 18:38:26 +0100

    Use temporary files to read and write everything
    These are deleted (manually) at the end. But the file name is generated for us.

--- X3GWriter.py
+++ X3GWriter.py
@@ -3,6 +3,8 @@
 
 import subprocess
 import os
+import tempfile
+import typing
 
 from UM.Mesh.MeshWriter import MeshWriter
 from UM.Logger import Logger
@@ -25,51 +27,67 @@ class X3GWriter(MeshWriter):
         scene = Application.getInstance().getController().getScene()
         gcode_list = getattr(scene, "gcode_list")
         if not gcode_list:
+            Logger.log("e", "There is no g-code to write.")
             return False
-
-        #Find an unused file name to temporarily write the g-code to.
-        file_name = stream.name
-        if not file_name: #Not a file stream.
-            Logger.log("e", "X3G writer can only write to local files.")
-            return False
-        file_directory = os.path.dirname(os.path.realpath(file_name)) #Save the tempfile next to the real output file.
-        i = 0
-        temp_file = os.path.join(file_directory, "output" + str(i) + ".gcode")
-        while os.path.isfile(temp_file):
-            i += 1
-            temp_file = os.path.join(file_directory, "output" + str(i) + ".gcode")
 
         #Write the g-code to the temporary file.
+        temp_gcode = None
         try:
-            with open(temp_file, "w", -1, "utf-8") as f:
-                for gcode in gcode_list:
-                    f.write(gcode)
-        except:
-            Logger.log("e", "Error writing temporary g-code file %s", temp_file)
-            _removeTemporary(temp_file)
+            temp_gcode = tempfile.NamedTemporaryFile("w", delete=False)
+            for gcode in gcode_list:
+                temp_gcode.write(gcode)
+            temp_gcode.close()
+            temp_cfg = None
+            try:
+                temp_cfg = tempfile.NamedTemporaryFile("w", delete=False)
+                self.write_cfg(temp_cfg)
+                temp_cfg.close()
+                temp_x3g = None
+                try:
+                    temp_x3g = tempfile.NamedTemporaryFile("r", delete=False)
+                    temp_x3g.close()
+                    command = self.gpx_command(temp_cfg.name, temp_gcode.name, temp_x3g.name)
+                    try:
+                        process = subprocess.Popen(command)
+                        process.wait() #Wait until it's done converting.
+                        output = process.communicate(b"y")
+                        Logger.log("d", str(output))
+                    except EnvironmentError as e:
+                        Logger.log("e", "System call to X3G converter application failed: {error_msg}".format(error_msg=str(e)))
+                        os.remove(temp_x3g.name)
+                        os.remove(temp_cfg.name)
+                        os.remove(temp_gcode.name)
+                        return False
+                    #Read from the temporary X3G file and put it in the stream.
+                    stream.write(open(temp_x3g.name, "rb").read().decode("Latin-1")) #Latin-1 just copies the binary data, pretending it is a string.
+
+                except EnvironmentError as e:
+                    if temp_x3g:
+                        Logger.log("e", "Error writing temporary X3G file {temp_x3g}: {error_msg}".format(temp_x3g=temp_x3g, error_msg=str(e)))
+                        os.remove(temp_x3g.name)
+                    else: #The NamedTemporaryFile constructor failed.
+                        Logger.log("e", "Error creating temporary X3G file: {error_msg}".format(error_msg=str(e)))
+                    os.remove(temp_x3g.name)
+                    os.remove(temp_cfg.name)
+                    os.remove(temp_gcode.name)
+                    return False
+            except EnvironmentError as e:
+                if temp_cfg:
+                    Logger.log("e", "Error writing temporary configuration file {temp_cfg}: {error_msg}".format(temp_cfg=temp_cfg, error_msg=str(e)))
+                    os.remove(temp_cfg.name)
+                else: #The NamedTemporaryFile constructor failed.
+                    Logger.log("e", "Error creating temporary configuration file: {error_msg}".format(error_msg=str(e)))
+                os.remove(temp_gcode.name)
+                return False
+        except EnvironmentError as e:
+            if temp_gcode:
+                Logger.log("e", "Error writing temporary g-code file {file_name}: {error_msg}".format(file_name = temp_gcode.name, error_msg=str(e)))
+                os.remove(temp_gcode.name)
+            else: #The NamedTemporaryFile constructor failed.
+                Logger.log("e", "Error creating temporary g-code file: {error_msg}".format(error_msg=str(e)))
             return False
 
-        #Call the converter application to convert it to X3G.
-        Logger.log("d", "App path: %s", os.getcwd())
-        Logger.log("d", "File name: %s", file_name)
-        binary_filename = self.gpx_executable()
-
-        command = [binary_filename, "-p", "-m", "r1d", "-c", os.path.join(os.path.dirname(os.path.realpath(__file__)), "cfg.ini"), temp_file, file_name]
-        safes = [os.path.expandvars(p) for p in command]
-        Logger.log("d", "Calling GPX: {command}".format(command=" ".join(command)))
-        stream.close() #Close the file so that the binary can write to it.
-        try:
-            process = subprocess.Popen(safes)
-            process.wait()
-            output = process.communicate(b"y")
-            Logger.log("d", str(output))
-        except Exception as e:
-            Logger.log("e", "System call to X3G converter application failed: %s", str(e))
-            _removeTemporary(temp_file)
-            return False
-
-        _removeTemporary(temp_file)
-        return True
+        return True #No exceptions.
 
     ##  Gets the location of the executable to run for converting to X3G.
     def gpx_executable(self):
@@ -86,8 +104,24 @@ class X3GWriter(MeshWriter):
         result = os.path.join(gpx_path, executable)
         result = os.path.expanduser(result)
         result = os.path.expandvars(result)
-        Logger.log("d", "GPX executable: {executable_file}".format(executable_file = result))
+        Logger.log("d", "GPX executable: {executable_file}".format(executable_file=result))
         return result
+
+    ##  Gets the command that we need to call GPX with.
+    #
+    #   \param configuration_file A file path to a configuration CFG file to run
+    #   GPX with.
+    #   \param gcode_file The input g-code file path.
+    #   \param x3g_file The output X3G file path.
+    #   \return A command to run GPX with, as list of parameters.
+    def gpx_command(self, configuration_file, gcode_file, x3g_file) -> typing.List[str]:
+        gpx_executable = self.gpx_executable()
+        result = [gpx_executable, "-c", configuration_file, gcode_file, x3g_file]
+        Logger.log("d", "GPX command: {command}".format(command=" ".join(result)))
+        return result
+
+    def write_cfg(self, cfg_stream):
+        pass #TODO: Write the CFG file.
 
 ##  Removes the temporary g-code file that is an intermediary result.
 #
